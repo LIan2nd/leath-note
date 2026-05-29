b80cade046b61df16cde77d359a8c216f7149085
LIan2nd <alfiannurusyaid19@gmail.com>
2026-05-29 07:44:08 +0700

    feat: add PWA support with manifest, service worker, and app registration

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+// @ts-nocheck
+const CACHE_NAME = 'leath-note-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json',
+  '/leath-note-logo.png',
+  '/textures/wood-background.png',
+  '/textures/leather-sidebar.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
